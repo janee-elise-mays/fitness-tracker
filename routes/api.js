1aefbd31eca2384266f742530503320e5c6e026e
Janeé Mays <janee.elise@gmail.com>
2021-05-22 15:50:41 -0400

    router.put, added skeleton info for router.get

--- routes/api.js
+++ routes/api.js
@@ -12,8 +12,27 @@ router.post("/api/workout", (req, res) => {
     });
 });
 
-router.put("/api/workout/", ({ body }, res) => {
-    
+router.put("/api/workout/:id", ({ body, params}, res) => {
+    db.Workout.findByIdAndUpdate(params.id,
+        {$push: {exercises: body}},
+        {new:true}
+     )
+        .then((workout) => {
+            res.status(200).json(workout);
+        })
+        .catch((err) => {
+            res.status(400).json(err.message);
+        });
+});
+
+router.get("/api/workout", (req, res) => {
+    db.Workout.find({})
+    .then((workout) => {
+    res.status(200).json(workout);
+    })
+    .catch((err) => {
+        res.status(400).json(err.message);
+    });
 });
 
 router.get("/api/workout", (req, res) => {
